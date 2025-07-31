ec42cabc778d46d0c53a1b2a95685e14b95bd4db
MiroslavMagerov <miroslav.magerov@gmail.com>
2025-07-31 14:11:16 +0200

    Almost finished navbar, added linear gradient text and border effects

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,22 +3,26 @@ import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <header className='navbar'>
-      <div className='navbar__div'>
-        <div className='navbar__main'>
-          <Code className='main__code-icon icon' />
-          <h3 className='main__title'>Mi Portfolio</h3>
+    <header className="navbar">
+      <div className="navbar__div">
+        <div className="navbar__main">
+          <Code className="main__code-icon icon" />
+          <h3 className="main__title">Mi Portfolio</h3>
         </div>
 
-        <ul className='navbar__list'>
-          <li className='list__list-item'>Sobre mí</li>
-          <li className='list__list-item'>Proyectos</li>
-          <li className='list__list-item'>Estudios</li>
-          <li className='list__list-item'>Contanto</li>
+        <ul className="navbar__list">
+          <li className="list__list-item">Sobre mí</li>
+          <li className="list__list-item">Proyectos</li>
+          <li className="list__list-item">Estudios</li>
+          <li className="list__list-item">Contacto</li>
         </ul>
-        <div className='navbar__cv-download'>
-          <a className='cv-download__button'>
-            <Download className='icon button__download-icon' size={15} />
+        <div className="navbar__cv-download">
+          <a className="cv-download__button">
+            <Download
+              strokeWidth={2.7}
+              className="icon button__download-icon"
+              size={15}
+            />
             CV
           </a>
         </div>
